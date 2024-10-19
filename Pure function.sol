6759cf1c0e01c2b30d87ae8abb84b133596897fd
Dev goyal <158808178+fafawds67685da@users.noreply.github.com>
2024-10-19 13:13:46 +0530

    Create Pure function.sol

---
+++ Pure function.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+contract ex{
+
+        uint a=10;
+    function abc (uint _a)  public pure returns (uint) {
+        _a=_a-1;
+        return _a;
+    }
+}
