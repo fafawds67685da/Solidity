9c95bce5aa3d40d2204509bdb94c67ec38a2085a
Dev goyal <158808178+fafawds67685da@users.noreply.github.com>
2024-10-19 13:20:20 +0530

    Create view function returning a local variable after changing it.sol

---
+++ view function returning a local variable after changing it.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+contract ex{
+
+        uint a=10;
+    function abc ()  public view returns (uint) {
+        uint _a= 1;
+        return _a;
+    }
+}
