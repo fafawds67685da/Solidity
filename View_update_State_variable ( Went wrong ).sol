57c68008d8171f96bfd899f4b781e5431422cc9b
Dev goyal <158808178+fafawds67685da@users.noreply.github.com>
2024-10-19 13:22:36 +0530

    Create View_update_State_variable ( Went wrong ).sol

---
+++ View_update_State_variable ( Went wrong ).sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+contract ex{
+
+        uint a=10;
+    function abc ()  public view returns (uint) {
+        a=a-5;
+        return a;
+    }
+}
